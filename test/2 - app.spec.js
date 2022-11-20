2e42cf412d04a8bd096bf937b2a06b45dea9a5fb
Airton Esmerio <aesmerio@gmail.com>
2022-11-20 15:29:17 +0000

    change the port to avoid conflict with Jenkins

--- test/2 - app.spec.js
+++ test/2 - app.spec.js
@@ -13,7 +13,7 @@ describe("App integration testing", function () {
 
   before(async () => {
     driver = await new Builder().forBrowser(Browser.CHROME).build()
-    await driver.get("http://localhost:8080")
+    await driver.get("http://localhost:8000")
     await driver.wait(until.titleIs("Lab 2 - Test Frameworks"), 10000)
   })
 
